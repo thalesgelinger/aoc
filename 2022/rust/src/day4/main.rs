146fd8d9e5e22c4c2dbb135e3945749f266c96a3
Thales Gelinger <thalesgelinger@gmail.com>
2023-09-10 16:02:54 -0300

    feat: add day4 part 2 rust

---
+++ 2022/rust/src/day4/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+
+fn main() {
+    let input: String = fs::read_to_string("values.txt").expect("Should inform file path");
+    let lines = input.lines();
+
+    let mut fully_contain_pairs = 0;
+
+    for line in lines {
+        let mut pairs = line.split(",");
+        let left = pairize_tuples(pairs.next().unwrap().to_owned());
+        let right = pairize_tuples(pairs.next().unwrap().to_owned());
+
+        fully_contain_pairs += match (left, right) {
+            ((l1, l2), (r1, r2)) if l1 <= r1 && r2 <= l2 => 1,
+            ((l1, l2), (r1, r2)) if r1 <= l1 && l2 <= r2 => 1,
+            ((l1, l2), (r1, _)) if l1 <= r1 && r1 <= l2 => 1,
+            ((l1, l2), (_, r2)) if l1 <= r2 && r2 <= l2 => 1,
+            ((l1, _), (r1, r2)) if r1 <= l1 && l1 <= r2 => 1,
+            ((_, l2), (r1, r2)) if r1 <= l2 && l2 <= r2 => 1,
+            _ => 0,
+        }
+    }
+
+    return println!("{fully_contain_pairs}");
+}
+
+fn pairize_tuples(str_pair: String) -> (usize, usize) {
+    let mut side = str_pair.split("-");
+
+    let first = side
+        .next()
+        .unwrap()
+        .parse::<usize>()
+        .expect("Why this is not a number?");
+
+    let second = side
+        .next()
+        .unwrap()
+        .parse::<usize>()
+        .expect("Why this is not a number?");
+
+    return (first, second);
+}
