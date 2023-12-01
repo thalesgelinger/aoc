485e169c2c8a86238469ed728b10976ff596fe7c
Thales Gelinger <thalesgelinger@gmail.com>
2023-12-01 17:53:41 -0300

    feat: aoc 2023 day 1 part 1

--- year-2023/src/main.swift
+++ year-2023/src/main.swift
@@ -1,9 +1,17 @@
 import Foundation
 
-guard let data = FileManager.default.contents(atPath: "example.txt") else {
-    fatalError("Error reading file")
-}
-guard let fileContents = String(data: data, encoding: .utf8) else {
-    fatalError("Error parsing content")
-}
-print("\(fileContents)")
+let data = FileManager.default.contents(atPath: "sample2.txt")!
+let fileContents = String(data: data, encoding: .utf8)!
+let lines = fileContents.split(separator: "\n")
+
+let digits = try Regex("[0-9]")
+
+let response = try lines.map{ w in 
+    let first = try digits.firstMatch(in: w)!
+    let last = try digits.firstMatch(in: String(w.reversed()))!
+    let val = Int("\(first.0)\(last.0)")!
+
+    return val
+ }.reduce(0) { (acc, el) in acc + el}
+
+print(response)
