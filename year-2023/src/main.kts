bc6bf8e63ddf1a58949cb04957194ce00c1e3e88
Thales Gelinger <thalesgelinger@gmail.com>
2023-12-04 20:21:46 -0300

    aoc 2023 day 4 part 1

--- year-2023/src/main.kts
+++ year-2023/src/main.kts
@@ -1,12 +1,36 @@
-
 import java.io.File
 
-val file = File("example.txt")
+val lines = File("input.txt").bufferedReader().lines()
 
-val bufferedReader = file.bufferedReader()
+var result = 0
 
-bufferedReader.useLines { lines ->
-    lines.forEach { line ->
-        println(line)
+lines.forEach {
+    val (_, tail) = it.split(":")
+    val numbers = tail.split(" ")
+    var checkWinners = false
+    var points = 0
+    var mapNum = HashMap<String, Boolean>()
+    numbers.forEach {
+        val el = it.trim()
+
+        if (el.equals("|")) checkWinners = true
+        if (!el.isEmpty()) {
+            when (checkWinners) {
+                false ->
+                        if (!mapNum.containsKey(el)) {
+                            mapNum[el] = true
+                        }
+                true ->
+                        if (mapNum.containsKey(el)) {
+                            when (points) {
+                                0 -> points = 1
+                                else -> points *= 2
+                            }
+                        }
+            }
+        }
     }
+    result += points
 }
+
+println("Result: $result")
