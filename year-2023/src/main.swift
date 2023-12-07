c109d4610781dfd187b63e5aca40c746de972012
Thales Gelinger <thalesgelinger@gmail.com>
2023-12-07 12:25:00 -0300

    aoc 2023 day 6 part 1 swift

--- year-2023/src/main.swift
+++ year-2023/src/main.swift
@@ -1,70 +1,28 @@
 import Foundation
 
-let data = FileManager.default.contents(atPath: "input2.txt")!
+let data = FileManager.default.contents(atPath: "input.txt")!
 let fileContents = String(data: data, encoding: .utf8)!
 let lines = fileContents.split(separator: "\n")
 
-let digits = try Regex("[0-9]")
+var times = lines[0].split(separator: " ")[1...]
+var distances = lines[1].split(separator: " ")[1...]
 
-let digitsMap = [
-    "one": 1, 
-    "two": 2, 
-    "three": 3, 
-    "four": 4, 
-    "five": 5, 
-    "six": 6, 
-    "seven": 7, 
-    "eight": 8, 
-    "nine": 9
-]
+var result = 1
 
-let response = try lines.map { l in
-    var line = String(l)
-    var head = "";
-    var tail = "";
-    var left = "";
-    var right = "";
+while times.count > 0 {
+    let totalTime = Int(times.popFirst()!)!
+    let currentMaxDistance = Int(distances.popFirst()!)!
 
-    for (i, le) in l.enumerated() {
-        if (left == ""){
-            head += String(le)
-
-            if le.isNumber {
-                left = String(le)
-            }
-
-            for (d, v) in digitsMap {
-                if head.contains(d) {
-                    left = String(v)
-                }
-            }
+    var countedWaysToWin = 0
+    for holdingTime in 1...totalTime {
+        let distance = (totalTime - holdingTime) * holdingTime
+        if (distance > currentMaxDistance){
+            countedWaysToWin += 1
         }
-        if (right == "") {
-            tail = String(l.suffix(i+1))
-
-            if tail.first!.isNumber {
-                right = String(tail.first!)
-            }
-
-            for (d, v) in digitsMap {
-                if tail.contains(d) {
-                    right = String(v)
-                    break
-                }
-            }
-        }
-
-        if( right != "" && left != "") {
-            break;
-        }
-
-
     }
 
-    return Int("\(left)\(right)")!
-}.reduce(0) { (acc, el) in acc + el}
+    result *= countedWaysToWin
 
+}
 
-
-print(response)
-
+print(result)
