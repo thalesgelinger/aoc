deae5a033ef572d6dca52929f48daa66d151e58b
Thales Gelinger <thales.gelinger@em.com>
2024-12-04 16:17:02 -0300

    2024 day 4 part 2

--- 2024/lua/days/04.lua
+++ 2024/lua/days/04.lua
@@ -16,16 +16,15 @@ for i, line in ipairs(lines) do
 end
 
 
---- @type table<[number,number]>
 local directions = {
-    { 1,  0 },  --horizontal-right
-    { -1, 0 },  --horizontal-left
-    { 0,  1 },  --vertical-down
-    { 0,  -1 }, --vertical-up
-    { 1,  -1 }, --diagnal-right-up
-    { 1,  1 },  --diagnal-right-down
-    { -1, 1 },  --diagnal-left-down
-    { -1, -1 }, --diagnal-left-up
+    {
+        { 1,  -1 }, --diagnal-right-up
+        { -1, 1 },  --diagnal-left-down
+    },
+    {
+        { 1,  1 },  --diagnal-right-down
+        { -1, -1 }, --diagnal-left-up
+    }
 }
 
 local function isValidCoord(x, y)
@@ -46,33 +45,35 @@ for l = 1, #map do
     for c = 1, #map[l] do
         local char = map[l][c]
 
-        if char == "X" then
-            map[l][c] = "-"
+        if char ~= "A" then goto continue end
 
-            for _, dir in ipairs(directions) do
-                local x, y = dir[1], dir[2]
+        local diagCount = 0
+        for _, dir in ipairs(directions) do
+            local from, to = dir[1], dir[2]
+            local fx, fy = l + from[1], c + from[2]
+            local tx, ty = l + to[1], c + to[2]
 
-                local mx, my = l + x, c + y
-
-                if not isValidCoord(mx, my) or map[mx][my] ~= "M" then
-                    goto continue
-                end
-
-                local ax, ay = mx + x, my + y
-                if not isValidCoord(ax, ay) or map[ax][ay] ~= "A" then
-                    goto continue
-                end
-
-                local sx, sy = ax + x, ay + y
-                if not isValidCoord(sx, sy) or map[sx][sy] ~= "S" then
-                    goto continue
-                end
-
-                result = result + 1
-
-                ::continue::
+            if not isValidCoord(fy, fx) or not isValidCoord(tx, ty) then
+                goto continue
             end
+
+            local charFrom = map[fx][fy]
+            local charTo = map[tx][ty]
+
+            local isMAS = charFrom == "S" and charTo == "M" or charFrom == "M" and charTo == "S"
+            if isMAS then
+                diagCount = diagCount + 1
+            end
+
+            ::continue::
         end
+
+
+        if diagCount == 2 then
+            result = result + 1
+        end
+
+        ::continue::
     end
 end
 
